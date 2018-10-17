56e6daae28c97e0d90a94b3057285bb364cf77e4
ksu_ven <ksu_ven@github.com>
2018-10-17 17:44:27 +0300

    Initial Commit

---
+++ fff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fff
+//
+//  Created by Ксения Венедиктова on 17.10.2018.
+//  Copyright © 2018 Ксения Венедиктова. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
