19fb29434d87ebe2fb534014db8753e2f2a9c3c4
ksu_ven <ksu_ven@github.com>
2018-10-17 19:27:36 +0300

    first task

--- fff/main.cpp
+++ fff/main.cpp
@@ -6,10 +6,70 @@
 //  Copyright © 2018 Ксения Венедиктова. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int a;
+int min (vector <int> b){
+    int minimum = b[0];
+    for (int i = 0; i< b.size(); i++){
+        if (b[i] < minimum){
+            minimum = b[i];
+        }
+    }
+    return minimum;
+}
+int all(vector<int>b){
+    static int c = 0;
+    int minimum = min(b);
+    if (minimum < b.size())
+    {
+        c+=minimum;
+        vector <int> d;
+        for (int i = 0; i<b.size(); i++){
+            if (b[i] > minimum){
+                d.push_back(b[i] - minimum);
+                if(i+1 < b.size()){
+                    if(b[i+1] == minimum){
+                        all(d);
+                        d.clear();
+                    }
+                    
+                }
+                else
+                {
+                    all(d);
+                }
+            }
+        }
+    }
+    else
+    {
+        c+=b.size();
+    }
+    return c;
+    
+}
+
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Введите количество досок" << endl;
+    cin >> a;
+    vector <int> vect;
+    for (int i = 0; i < a; i++) {
+        int c;
+        cout << "Введите высоту" << i+1 << "доски" << endl;
+        cin >> c;
+        vect.push_back(c);
+        }
+    int ans = all(vect);
+    cout << ans;
+    
     return 0;
 }
